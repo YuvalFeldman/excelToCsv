0acfb2eab66b1ea24fcb8825e10bb97604cdbe12
Yuval Feldman <yuval.fe@gigya.net>
2016-12-21 13:38:13 +0200

    init

---
+++ ExcelToCsv/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ExcelToCsv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Convert = new System.Windows.Forms.Button();
+            this.BatchConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(12, 12);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(112, 28);
+            this.Convert.TabIndex = 0;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // BatchConvert
+            // 
+            this.BatchConvert.Location = new System.Drawing.Point(12, 46);
+            this.BatchConvert.Name = "BatchConvert";
+            this.BatchConvert.Size = new System.Drawing.Size(112, 31);
+            this.BatchConvert.TabIndex = 1;
+            this.BatchConvert.Text = "Batch";
+            this.BatchConvert.UseVisualStyleBackColor = true;
+            this.BatchConvert.Click += new System.EventHandler(this.BatchConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(135, 84);
+            this.Controls.Add(this.BatchConvert);
+            this.Controls.Add(this.Convert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.Button BatchConvert;
+    }
+}
+
---
+++ ExcelToCsv/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.IO;
+
+namespace ExcelToCsv
+{
+    public partial class Form1 : Form
+    {
+
+        private OpenFileDialog _openFileDialog;
+        private SaveFileDialog _saveFileDialog;
+        private FolderBrowserDialog _folderBrowserDialog;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeReaders();
+        }
+        private void InitializeReaders()
+        {
+            _openFileDialog = new OpenFileDialog { Filter = @"Excel Files(.xlsx)|*.xlsx| Excel Files(.xls)|*.xls| Excel Files(.xlsm)|*.xlsm" };
+            _saveFileDialog = new SaveFileDialog { Filter = @"CSV files (*.csv)|*.csv" };
+            _folderBrowserDialog = new FolderBrowserDialog();
+        }
+
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            string originUrl;
+            string targetUrl;
+
+            if (_openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originUrl = _openFileDialog.FileName;
+            }
+            else
+            {
+                return;
+            }
+
+            if (_saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                targetUrl = _saveFileDialog.FileName;
+            }
+            else
+            {
+                return;
+            }
+            try
+            {
+                ConvertExcelToCsv(originUrl, targetUrl);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"There was an error in the request, could not complete the conversion");
+                return;
+            }
+
+            MessageBox.Show(@"Action completed successfully");
+
+        }
+
+        private void BatchConvert_Click(object sender, EventArgs e)
+        {
+            string[] rawFiles;
+            string saveDir;
+
+            if (_folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                rawFiles = Directory.GetFiles(_folderBrowserDialog.SelectedPath);
+            }
+            else
+            {
+                return;
+            }
+            if (_folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                saveDir = _folderBrowserDialog.SelectedPath;
+            }
+            else
+            {
+                return;
+            }
+            try
+            {
+                foreach (var originUrl in rawFiles)
+                {
+                    var extension = Path.GetExtension(originUrl);
+                    if (extension == null || (!extension.Equals(".xlsx") && !extension.Equals(".xls") && !extension.Equals(".xlsm"))) continue;
+                    var targetUrl = saveDir + @"\" + Path.GetFileNameWithoutExtension(originUrl) + ".csv";
+                    ConvertExcelToCsv(originUrl, targetUrl);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"There was an error in the request, could not complete the conversion");
+                return;
+            }
+
+            MessageBox.Show(@"Action completed successfully");
+        }
+
+        private void ConvertExcelToCsv(string excelFilePath, string csvOutputFile)
+        {
+            ValidateFileNonExistance(csvOutputFile);
+            using (var p = new ExcelPackage(new FileInfo(excelFilePath)))
+            {
+                var sheet = p.Workbook.Worksheets[1];
+                using (var writer = File.CreateText(csvOutputFile))
+                {
+                    var rowCount = sheet.Dimension.End.Row;
+                    var columnCount = sheet.Dimension.End.Column;
+                    for (var r = 1; r <= rowCount; r++)
+                    {
+                        for (var c = 1; c <= columnCount; c++)
+                        {
+                            writer.Write(sheet.Cells[r, c].Value);
+                            writer.Write(",");
+                        }
+                        writer.WriteLine();
+                    }
+                }
+            }
+        }
+
+        private void ValidateFileNonExistance(string csvOutputFile)
+        {
+            if (File.Exists(csvOutputFile)) File.Delete(csvOutputFile);
+        }
+    }
+}
